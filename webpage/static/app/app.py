9415b8e8a4e787341ffe9e2b16bc56c1a3cfd46e
danielbrock4 <daniel.brock4@outlook.com>
2021-10-28 14:29:52 -0500

    deloying text files to test Heroku

--- webpage/static/app/app.py
+++ webpage/static/app/app.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # @app.route('/get-help')
 # def get_help():
