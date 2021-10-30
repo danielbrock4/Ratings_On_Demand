11a6c9d7a6698e74fe056105cf9f17bd26b52c68
danielbrock4 <daniel.brock4@outlook.com>
2021-10-30 18:11:30 -0500

    playing around with enviroment structure

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,9 @@
 #import dependencies and use Flask to render a template
 from flask import Flask
 
-
 # Set Up Flask
     # To define our Flask app we will create a Flask application called "app."
 app = Flask(__name__)
 
-from application import routes
+import routes
 
