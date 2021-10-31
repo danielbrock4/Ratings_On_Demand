502877bb1ea4cbf7a6e6bd157944ce929f7b852e
danielbrock4 <daniel.brock4@outlook.com>
2021-10-30 20:49:23 -0500

    studded webpage

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 # import dependencies
 from application import app
-#import dependencies and use Flask to render a template, redirecting to another url, and creating a URL
+# import dependencies and use Flask to render a template, redirecting to another url, and creating a URL
 from flask import render_template, url_for, redirect, jsonify
 import pandas as pd
 from pandas import DataFrame, read_csv
@@ -9,7 +9,27 @@ import json
 import plotly
 import plotly.express as px
 
+# create engine to pull in data
+engine = create_engine('postgresql+psycopg2://postgres:moviesondemand@moviesondemandaws.cfwjiare7kds.us-east-2.rds.amazonaws.com:5432/postgres')
+# main_df = pd.read_sql_table('consolidated_pre_transformation', con=engine)
+
 
 @app.route("/")
 def index():
-    return render_template("layout.html", title = "Home")
+     # Graph One
+    df = px.data.medals_wide()
+    fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input")
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph two
+    df = px.data.iris()
+    fig2 = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species',  title="Iris Dataset")
+    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph three
+    df = px.data.gapminder().query("continent=='Oceania'")
+    fig3 = px.line(df, x="year", y="lifeExp", color='country',  title="Life Expectancy")
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template("index.html", title = "Home", graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON)
