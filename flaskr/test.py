fea67ab7fcaea6ea3798ba5b962b49c6315791f0
danielbrock4 <daniel.brock4@outlook.com>
2021-11-15 10:36:26 -0600

    path not working, but testing it on heroku

--- flaskr/test.py
+++ flaskr/test.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 import plotly
 import plotly.express as px
+import os
 
-ridge_test_results = r'C:/Users/Daniel Brock/OneDrive/Desktop/DataAnalyticsBootcamp/Final_Project/Ratings_On_Demand/flaskr/static/ridge_test_results.csv'
 
-clean_comp_results = pd.read_csv(ridge_test_results, low_memory=False)
+ridge_test_results  = os.path.join("static" , "ridge_test_results.csv")
+# ridge_test_results = r'ridge_test_results.csv'
+
+# ridge_test_results = r'C:/Users/Daniel Brock/OneDrive/Desktop/DataAnalyticsBootcamp/Final_Project/Ratings_On_Demand/flaskr/static/ridge_test_results.csv'
+
+df = pd.read_csv(ridge_test_results, low_memory=False)
